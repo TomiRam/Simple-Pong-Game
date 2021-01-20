0d350e0543377088cf0cd6a8b3d9095cfb256172
Tomi Rämö <47462643+TomiRam@users.noreply.github.com>
2021-01-20 12:18:15 +0200

    Add files via upload

---
+++ Assets/Paddle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    public KeyCode up;
+    public KeyCode down;
+    Rigidbody2D rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(up))
+        {
+            rigidBody.velocity = new Vector2(0f, 7f);
+        }
+        else if (Input.GetKey(down))
+        {
+            rigidBody.velocity = new Vector2(0f, -7f);
+        }
+        else
+        {
+            rigidBody.velocity = new Vector2(0f, 0f);
+        }
+    }
+}
+	
+	
